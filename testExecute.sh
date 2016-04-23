ccaf83da32c35d5fc1e6713fe71f0cdf643b9515
jhaocheng.wu <jhaocheng.wu@gmail.com>
2016-04-23 16:52:59 +0800

    test

---
+++ testExecute.sh
@@ -0,0 +1,10 @@
+# Do not uncomment these lines to directly execute the script
+# Modify the path to fit your need before using this script
+#hdfs dfs -rm -r /user/TA/WordCount/Output/
+#hadoop jar WordCount.jar wordcount.WordCount /user/shared/WordCount/Input /user/TA/WordCount/Output
+#hdfs dfs -cat /user/TA/WordCount/Output/part-*
+input_dir=/user/s104061516/InvertedIndex/testInput
+output_dir=/user/s104061516/InvertedIndex/testOutput
+hdfs dfs -rm -r $output_dir
+hadoop jar InvertedIndex.jar part3.InvertedIndex  $input_dir $output_dir
+hdfs dfs -cat $output_dir/part-*
---
+++ testExecute2.sh
@@ -0,0 +1,22 @@
+# Do not uncomment these lines to directly execute the script
+# Modify the path to fit your need before using this script
+#hdfs dfs -rm -r /user/TA/WordCount/Output/
+#hadoop jar WordCount.jar wordcount.WordCount /user/shared/WordCount/Input /user/TA/WordCount/Output
+#hdfs dfs -cat /user/TA/WordCount/Output/part-*
+input_dir=/user/s104061516/InvertedIndex/testOutput
+output_dir=/user/s104061516/InvertedIndex/testTableOutput
+hdfs dfs -rm -r $output_dir
+hadoop jar BuildTable.jar part3.InvertedIndex  $input_dir $output_dir
+hdfs dfs -cat $output_dir/part-*
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+
